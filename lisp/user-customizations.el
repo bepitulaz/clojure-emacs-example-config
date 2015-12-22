1019130ffd4213541ae146b003c54bba1b70572e
Asep Bagja Priandana <bepitulaz@gmail.com>
2015-12-22 10:41:59 +0700

    latest setup with swift support

--- lisp/user-customizations.el
+++ lisp/user-customizations.el
@@ -6,3 +6,25 @@
 (load-theme 'misterioso)
 
 (global-linum-mode 1)
+
+;; Activate web mode for editing html, js, and css.
+(require 'web-mode)
+
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+;; Set the indentation default to 2 in web mode
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2))
+
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+
+;; Flychecker for Swift Mode
+(add-to-list 'flycheck-checkers 'swift)
